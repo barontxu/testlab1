14e09e0eafff53f60819851b37ed8c8e84971dd3
Tanis <xuzzbaront@gmail.com>
2015-06-12 21:33:13 +0800

    second commit

--- libs/error.h
+++ libs/error.h
@@ -7,8 +7,7 @@
 #define E_INVAL                3    // Invalid parameter
 #define E_NO_MEM            4    // Request failed due to memory shortage
 #define E_NO_FREE_PROC        5    // Attempt to create a new process beyond
-#define E_FAULT                6    // Memory fault
-
+#define E_FAULT                6    
 /* the maximum allowed */
 #define MAXERROR            6
 
